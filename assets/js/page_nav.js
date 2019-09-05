1f92497a1e5b62055d5e563b038ce296cd3f2cde
Harry <mozhx020@163.com>
2019-09-05 22:58:04 +0800

    no message

--- assets/js/page_nav.js
+++ assets/js/page_nav.js
@@ -1,5 +1,6 @@
 function pageGo(total) {
     var pageNum = document.getElementById("page_input").value;
+    _hmt.push(['_trackEvent', 'pageIndex', 'clickGO', pageNum, 1])
     pageNum = pageNum.replace(/\b(0+)/gi, "");
     var action;
     if (pageNum <= 1) {
